eb6a59bdb31f6dd45663dcd1d6a7bf673056f068
ajp1016 <ajp1016@sru.edu>
2017-12-10 20:52:37 -0500

    small change
    unused import and variable for NodesLL

--- Zeus/src/edu/sru/thangiah/zeus/metaheuristics/evolutionary/TSPCostFunction.java
+++ Zeus/src/edu/sru/thangiah/zeus/metaheuristics/evolutionary/TSPCostFunction.java
@@ -1,35 +1,28 @@
 package edu.sru.thangiah.zeus.metaheuristics.evolutionary;
 
-import edu.sru.thangiah.zeus.core.Depot;
 import edu.sru.thangiah.zeus.core.DepotLinkedList;
 import edu.sru.thangiah.zeus.core.Nodes;
 import edu.sru.thangiah.zeus.core.NodesLinkedList;
 import edu.sru.thangiah.zeus.core.ProblemInfo;
-import edu.sru.thangiah.zeus.core.Settings;
-import edu.sru.thangiah.zeus.core.Truck;
 import edu.sru.thangiah.zeus.tsp.*;
 
 public class TSPCostFunction extends AbstractFitnessFunction
 {
-	static TSPNodesLinkedList StartList = new TSPNodesLinkedList();
-	TSPDepotLinkedList depotList;
-	TSPNodesLinkedList nodes;
-	public TSPCostFunction(TSPDepotLinkedList depotIn)
+	static NodesLinkedList StartList = new TSPNodesLinkedList();
+	DepotLinkedList depotList;
+	NodesLinkedList nodes;
+	public TSPCostFunction(DepotLinkedList depotIn)
 	{
 		depotList = depotIn;
-		nodes = (TSPNodesLinkedList) depotList.getHead().getNext().getMainTrucks().getHead().getNext().getMainNodes();
+		nodes = (NodesLinkedList) depotList.getHead().getNext().getMainTrucks().getHead().getNext().getMainNodes();
 	}
 	
-	public double evaluate(IChromosome chrome)
-	{
-		//Settings.printDebug(Settings.COMMENT, Integer.toString(chrome.size()));
+	public double evaluate(IChromosome chrome){
 	    NodesLinkedList newList = (NodesLinkedList) StartList.clone();
-	    //Settings.printDebug(Settings.COMMENT, Integer.toString(chrome.size()));
-	    //Settings.printDebug(Settings.COMMENT, Integer.toString(nodes.getSize()));
-	    for (int i = 0; i < chrome.size(); i++)
-	    {
+
+	    for (int i = 0; i < chrome.size(); i++){
 	    	int index = (int) chrome.getGene(i).getInternalValue() + 1; //+1 Zeus stores indexes from 1 not 0
-	    	TSPNodes nodey = new TSPNodes(); 
+	    	Nodes nodey = new Nodes(); 
 	    	nodey.setShipment(nodes.getNodeByIndex(index).getShipment());
 	    	newList.insertNodeLast(nodey);
 	    }
